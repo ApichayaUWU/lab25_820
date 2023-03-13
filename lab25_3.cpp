3994ed4ee1e130991dc636894ae8812ad4697a90
FAH <apichaya.inkhiewsai@gmail.com>
2023-03-13 23:57:32 +0700

    complete

--- lab25_3.cpp
+++ lab25_3.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
+#include<set>
 
 using namespace std;
 
-//Write function count() here.
+int count(int d[],int N)
+{
+	set<int> c;
+	for(int i = 0 ; i< N ;i++)
+	{
+		c.insert(d[i]);
+	}
+	return c.size();
+
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
